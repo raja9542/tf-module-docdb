402f1206bd846fde44c641b33e3e574ad4694bac
raja9542 <rajasekhar1banda@gmail.com>
2023-11-13 19:04:43 +0530

    docdb cluster instance creation

--- main.tf
+++ main.tf
@@ -49,3 +49,15 @@ resource "aws_docdb_cluster" "docdb" {
     {Name = "${var.env}-docdb-cluster"}
   )
 }
+
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+  count              = var.number_of_instances
+  identifier         = "${var.env}-docdb-cluster-instance-${count.index+1}"
+  cluster_identifier = aws_docdb_cluster.docdb.id
+  instance_class     = var.instance_class
+
+  tags = merge(
+    local.common_tags,
+    {Name = "${var.env}-docdb-cluster-instance-${count.index+1}"}
+  )
+}
