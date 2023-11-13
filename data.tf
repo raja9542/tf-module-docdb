9496188c6d654a443a38d95ee62dfa1a426f1b42
raja9542 <rajasekhar1banda@gmail.com>
2023-11-13 20:13:42 +0530

    storage encryption in document db

--- data.tf
+++ data.tf
@@ -7,5 +7,5 @@ data "aws_ssm_parameter" "DB_ADMIN_PASS" {
 }
 
 data "aws_kms_key" "roboshop" {  // roboshop is a alias name given in key management service
-  key_id = "roboshop"
+  key_id = "alias/roboshop"
 }
